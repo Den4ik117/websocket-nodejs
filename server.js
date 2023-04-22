c1ddf4271a6e588dd5eb50aa383b271fb936ab67
Denis <gaz91.91@mail.ru>
2023-04-22 23:11:25 +0500

    First commit with simple nodejs websocket server

---
+++ server.js
@@ -0,0 +1,41 @@
+const WebSocket = require('ws');
+
+const wsServer = new WebSocket.WebSocketServer({
+    host: '127.0.0.1',
+    port: 9000
+});
+
+wsServer.on('connection', onConnect);
+
+function onConnect(wsClient) {
+    console.log('Новый пользователь');
+    // отправка приветственного сообщения клиенту
+    wsClient.send('Привет');
+    wsClient.on('message', function(message) {
+        try {
+            // сообщение пришло текстом, нужно конвертировать в JSON-формат
+            const jsonMessage = JSON.parse(message);
+            switch (jsonMessage.action) {
+                case 'ECHO':
+                    wsClient.send(jsonMessage.data);
+                    break;
+                case 'PING':
+                    setTimeout(function() {
+                        wsClient.send('PONG');
+                    }, 2000);
+                    break;
+                default:
+                    console.log('Неизвестная команда');
+                    break;
+            }
+        } catch (error) {
+            console.log('Ошибка', error);
+        }
+    });
+    wsClient.on('close', function() {
+        // отправка уведомления в консоль
+        console.log('Пользователь отключился');
+    });
+}
+
+console.log('Сервер запущен на 127.0.0.1:9000');
